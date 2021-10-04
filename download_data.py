5c36f6f05facef24c9b4f7a57c100d845654fe94
hamzarabi3 <googlinghamza@gmail.com>
2021-10-04 15:28:39 +0000

    download only epl data

--- download_data.py
+++ download_data.py
@@ -1,113 +1,84 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 22 13:39:39 2021
-
-"""
 import pandas as pd
 import os
 
-# we create a dictionary dict_countries similar to the one we used in the scraping section
-# but in this case we need to specify the names considered in the link to download the csv e.g. "SP1" (Spanish League)
-dict_countries = {
-    "Spanish La Liga": "SP1",
-    "Spanish Segunda Division": "SP2",
-    "German Bundesliga": "D1",
-    "German Bundesliga 2": "D2",
-    "Italian Serie A": "I1",
-    "Italian Serie B": "I2",
-    "English Premier League": "E0",
-    "English League 1": "E2",
-    "English League 2": "E3",
-    "French Ligue 1": "F1",
-    "French Ligue 2": "F2",
-    "Dutch Eredivisie": "N1",
-    "Belgian First Division A": "B1",
-    "Portuguese Primeira Liga": "P1",
-    "Turkish Super League": "T1",
-    "Greek Super League": "G1",
-}
+from pandas.core import frame
 
-# dict_historical_data contains data of the past 5 years. we'll use it to manage 2 dataframes: df_historical_data and df_profile
-dict_historical_data = {}
+data_folder='data'
+league='E0'
 
-# to download all the leagues we loop through the dictionary
-for league in dict_countries:
-    frames = []
-    for i in range(10, 21):
-        try:
-            df = pd.read_csv(
-                "http://www.football-data.co.uk/mmz4281/"
-                + str(i)
-                + str(i + 1)
-                + "/"
-                + dict_countries[league]
-                + ".csv"
-            )
-        except:  # Italian Serie B (0xa0 utf-8)
-            df = pd.read_csv(
-                "http://www.football-data.co.uk/mmz4281/"
-                + str(i)
-                + str(i + 1)
-                + "/"
-                + dict_countries[league]
-                + ".csv",
-                encoding="unicode_escape",
-            )
+frames=[]
+#split into two chunks
+#before 2000
+
+start_season0=93
+end_season0=99
+for i in range(start_season0, end_season0):
+    start="0"+str(i) if i <10 else str(i)
+    end="0"+str(i+1) if i+1<10 else str(i+1)
+    try:
+        df = pd.read_csv(
+            "http://www.football-data.co.uk/mmz4281/"
+            + start
+            + end
+            + "/"
+            + league
+            + ".csv"
+        )
         df = df.assign(season=i)
+        print(f'season {start}/{end} shape is {df.shape}')
+
         frames.append(df)
-    df_frames = pd.concat(frames)
-    df_frames = df_frames.rename(
-        columns={
-            "Date": "date",
-            "HomeTeam": "home_team",
-            "AwayTeam": "away_team",
-            "FTHG": "home_goals",
-            "FTAG": "away_goals",
-        }
+    except:
+        print(f'season {start}/{end} not read')
+#1999/2000 season:
+start=str(99)
+end='00'
+try:
+    df = pd.read_csv(
+        "http://www.football-data.co.uk/mmz4281/"
+        + start
+        + end
+        + "/"
+        + league
+        + ".csv"
     )
-    dict_historical_data[league] = df_frames
+    df = df.assign(season=i)
+    print(f'season {start}/{end} shape is {df.shape}')
+    frames.append(df)
+except:
+    print(f'season {start}/{end} not read')
 
 
-MLS2020 = pd.read_csv("https://www.football-data.co.uk/new/USA.csv")
-Swed2020 = pd.read_csv("https://www.football-data.co.uk/new/SWE.csv")
-Nor2020 = pd.read_csv("https://www.football-data.co.uk/new/NOR.csv")
-LigaMx2020 = pd.read_csv("https://www.football-data.co.uk/new/MEX.csv")
-Arg = pd.read_csv("https://www.football-data.co.uk/new/ARG.csv")
-AUT = pd.read_csv("https://www.football-data.co.uk/new/AUT.csv")
-BRA = pd.read_csv("https://www.football-data.co.uk/new/BRA.csv")
-CHN = pd.read_csv("https://www.football-data.co.uk/new/CHN.csv")
-FIN = pd.read_csv("https://www.football-data.co.uk/new/FIN.csv")
+start_season1=0
+end_season1=21
 
-Other_Leagues = pd.concat(
-    [MLS2020, Swed2020, Nor2020, LigaMx2020, Arg, AUT, BRA, CHN, FIN]
-)
+#after 2000
+for i in range(start_season1, end_season1):
+    start="0"+str(i) if i <10 else str(i)
+    end="0"+str(i+1) if i+1<10 else str(i+1)
+    try:
+        df = pd.read_csv(
+            "http://www.football-data.co.uk/mmz4281/"
+            + start
+            + end
+            + "/"
+            + league
+            + ".csv"
+        )
+        df = df.assign(season=i)
+        print(f'season {start}/{end} shape is {df.shape}')
+        frames.append(df)
+    except:
+        print(f'season {start}/{end} shape is {df.shape}')
 
-Other_Leagues.Date = pd.to_datetime(Other_Leagues.Date)
-Other_Leagues = Other_Leagues.rename(
-    columns={
-        "Date": "date",
-        "Home": "home_team",
-        "Away": "away_team",
-        "HG": "home_goals",
-        "AG": "away_goals",
-        "Res": "FTR",
-    }
-)
 
-leagues = list(dict_historical_data.keys())
-sets = [set(dict_historical_data[l].columns) for l in leagues] + [
-    set(Other_Leagues.columns)
-]
-common_columns = set.intersection(*sets)
+sets=[set(frame.columns) for frame in frames]
+to_keep=list(set.intersection(*sets))
+frames=[frame[to_keep] for frame in frames]
+for frame in frames:
+    print(frame.shape)
+df_frames = pd.concat(frames)
 
-data_file = os.path.join("data", "all_leagues.csv")
-
-league = dict_historical_data[leagues[0]][common_columns]
-for l in leagues[1:]:
-    df = dict_historical_data[l][common_columns]
-    league = league.append(df)
-
-league = league.append(Other_Leagues[common_columns])
-to_keep = ["away_team", "home_team", "date", "FTR", "home_goals", "away_goals"]
-league = league[to_keep]
-league.to_csv(data_file)
+save_as=os.path.join(data_folder,f'match_results_{league}_{start_season0}_{end_season1}.csv')
+df_frames.to_csv(save_as)
+    
